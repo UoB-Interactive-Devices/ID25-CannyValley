229dfc9eaa01b2f8d147d3031727c16eef6c3079
yd22255 <123549523+yd22255@users.noreply.github.com>
2025-02-26 12:17:49 +0000

    updated changes to the rmm function
    added the canva questionnaire

--- Mimir/mimir.py
+++ Mimir/mimir.py
@@ -9,9 +9,7 @@ def playmusic(number):
 
 def dosensors():
     x=0
-    y=0
-    z=0
-    return x,y,z
+    return x
 
 def lightsUp():
     print("lights going up")
@@ -51,7 +49,8 @@ def describeEnvironment(number):
         if theinput == "yes" or theinput == "yeah" or theinput == "y" or theinput == "OK":
             thisenv = random.randint(0,2)
             thisenv=0
-            print("Welcome to the "+str(hypeenvironments[thisenv][0]))
+            placename = str(hypeenvironments[thisenv][0])
+            print("Welcome to the",placename)
             time.sleep(1)
             thesetimes = random.randint(4,6)
             for i in range(thesetimes):
@@ -76,7 +75,8 @@ def describeEnvironment(number):
         if theinput == "yes" or theinput == "yeah" or theinput == "y" or theinput == "OK":
             thisenv = random.randint(0,2)
             thisenv=0
-            print("Welcome to the "+str(calmenvironments[thisenv][0]))
+            placename = str(calmenvironments[thisenv][0])
+            print("Welcome to the "+placename)
             time.sleep(1)
             thesetimes = random.randint(4,6)
             for i in range(thesetimes):
@@ -100,12 +100,9 @@ myaffirmations = [[[1,3],"Today is my day!"], [[-2,1], "I am enough"], [[-3,2], 
 
 def findanaffirmation(number):
     randomaff = random.randint(0,len(myaffirmations)-1)
-    print(randomaff)
     number1 = myaffirmations[randomaff][0][0]
     number2 = myaffirmations[randomaff][0][1]
-    print(myaffirmations[randomaff][0][0],number, myaffirmations[randomaff][0][1])
     if number1<=number<=number2:
-        print("the affirmation in faaf: "+myaffirmations[randomaff][1])
         return (myaffirmations[randomaff][1])
     else:
         labouredaff=findanaffirmation(number)
@@ -132,7 +129,7 @@ def positiveAffirmations(number):
         if theinput == "yes" or theinput == "yeah" or theinput == "y" or theinput == "OK":
             thisaff = findanaffirmation(number)
             saytheaffirmation(thisaff, 0)
-        #say some hype shit idk
+        
         elif theinput == "no" or theinput == "n" or theinput == "nah":
             print ("OK, we'll do something else.")
         else:
@@ -142,9 +139,8 @@ def positiveAffirmations(number):
         theinput=input ("Let's get into it, time to shout back after me ")
         if theinput == "yes" or theinput == "yeah" or theinput == "y" or theinput == "OK":
             thisaff = findanaffirmation(number)
-            print("the afirmation: ",thisaff)
             saytheaffirmation(thisaff, 0)
-        #say some pretty hype shit
+        
         elif theinput == "no" or theinput == "n" or theinput == "nah":
             print ("OK, we'll do something else.")
         else:
@@ -155,25 +151,147 @@ def positiveAffirmations(number):
         if theinput == "yes" or theinput == "yeah" or theinput == "y" or theinput == "OK":
             thisaff = findanaffirmation(number)
             saytheaffirmation(thisaff, 0)
-        #say some mildly hype shit
+        
         elif theinput == "no" or theinput == "n" or theinput == "nah":
             print ("OK, we'll do something else.")
         else:
             print ("I didn't quite get that. Try again? ")
             positiveAffirmations(number)
 
+def highhrquestions():
+    q1R=0
+    while q1R==0:
+        q1 = input("Does your energy feel focused or scattered? ")
+        if(q1 == "Focused" or q1 == "focused"):
+            q1R=1
+        elif (q1 == "Scattered" or q1 == "scattered"):
+            q1R=2
+        else:
+            print("can we try that again? I didn't quite catch that.")
+    q2R = 0
+    while q2R==0:
+        q2 = input("Is your energy driven by excitement or purpose? ")
+        if(q2 == "Excitement" or q2 == "excitement"):
+            q2R=1
+        elif (q2 == "Purpose" or q2 == "purpose"):
+            q2R=2
+        else:
+            print("can we try that again? I didn't quite catch that.")
+    if q1R==1 and q2R==1:
+        q3R = 0
+        while q3R==0:
+            q3 = input("Is your focus fueled by a sense of urgency or long-term ambition? ")
+            if(q3 == "Urgency" or q3 == "urgency"):
+                return(2)
+            elif (q3 == "Ambition" or q3 == "ambition" or q3 == "Long-Term Ambition" or q3 == "long-term ambition"):
+                return(1)
+            else:
+                print("can we try that again? I didn't quite catch that.")
+    elif q1R==1 and q2R==2:
+        q3R = 0
+        while q3R==0:
+            q3 = input("Are you actively working toward something or just feeling positive? ")
+            if(q3 == "Working" or q3 == "working" or q3 == "Working Towards Something" or q3 == "working towards something"):
+                return(2)
+            elif (q3 == "Feeling Positive" or q3 == "feeling positive"):
+                return(0)
+            else:
+                print("can we try that again? I didn't quite catch that.")
+    elif q1R==2 and q2R==1:
+        q3R = 0
+        while q3R==0:
+            q3 = input("Is your excitement more about movement or about looking forward to something? ")
+            if(q3 == "looking forward" or q3 == "Looking Forward" or q3 == "Looking Forward To Something" or q3 == "looking forward to something"):
+                return(1)
+            elif (q3 == "Movement" or q3 == "movement"):
+                return(3)
+            else:
+                print("can we try that again? I didn't quite catch that.")
+    elif q1R==2 and q2R==2:
+        q3R = 0
+        while q3R==0:
+            q3 = input("Is your energy making it hard to stay still or just making you feel the same?")
+            if(q3 == "hard to stay still" or q3 == "Hard to Stay Still"):
+                return(3)
+            elif (q3 == "Feel the Same" or q3 == "feel the same"):
+                return(0)
+            else:
+                print("can we try that again? I didn't quite catch that.")
+
+def lowhrquestions():
+    q1R=0
+    while q1R==0:
+        q1 = input("Do you feel more calm or tired? ")
+        if(q1 == "Calm" or q1 == "calm"):
+            q1R=1
+        elif (q1 == "Tired" or q1 == "tired"):
+            q1R=2
+        else:
+            print("can we try that again? I didn't quite catch that.")
+    q2R = 0
+    while q2R==0:
+        q2 = input("Do you think you have a normal amount of energy, or lower than normal? ")
+        if(q2 == "Normal" or q2 == "normal"):
+            q2R=1
+        elif (q2 == "Lower" or q2 == "lower" or q2 == "Lower than Normal" or q2 == "lower than normal"):
+            q2R=2
+        else:
+            print("can we try that again? I didn't quite catch that.")
+    if q1R==1 and q2R==1:
+        q3R = 0
+        while q3R==0:
+            q3 = input("Do you feel satisfied or there could be small improvements? ")
+            if(q3 == "Satisfied" or q3 == "satisfied"):
+                return(-1)
+            elif (q3 == "Small Improvements" or q3 == "small improvements"):
+                return(0)
+            else:
+                print("can we try that again? I didn't quite catch that.")
+    elif q1R==1 and q2R==2:
+        q3R = 0
+        while q3R==0:
+            q3 = input("Do you feel a quiet sense of happiness or more at ease? ")
+            if(q3 == "Happiness" or q3 == "happiness" ):
+                return(-1)
+            elif (q3 == "At Ease" or q3 == "at ease"):
+                return(-2)
+            else:
+                print("can we try that again? I didn't quite catch that.")
+    elif q1R==2 and q2R==1:
+        q3R = 0
+        while q3R==0:
+            q3 = input("Do you feel drained or could you carry on with your current activity? ")
+            if(q3 == "Drained" or q3 == "drained"):
+                return(-3)
+            elif (q3 == "I can carry on" or q3 == "Carry On" or q3 == "carry on"):
+                return(0)
+            else:
+                print("can we try that again? I didn't quite catch that.")
+    elif q1R==2 and q2R==2:
+        q3R = 0
+        while q3R==0:
+            q3 = input("Do you feel like not doing any activity or simply being present in the moment?")
+            if(q3 == "Not doing any activity" or q3 == "Not Doing any Activity"):
+                return(-3)
+            elif (q3 == "Just Being Present" or q3 == "just being present"):
+                return(-1)
+            else:
+                print("can we try that again? I didn't quite catch that.")
+
 def rmm ():
-    userfeeling = input ("how are you feeling on a scale from -3 to 3 ")
-    x,y,z=dosensors()
-    user_mood = round((0.6*float(userfeeling))+(0.4*(x*y*z)))
+    heartrate=dosensors()
+    if(heartrate>50):
+        userfeeling=highhrquestions()
+    elif(heartrate<=50):
+        userfeeling=lowhrquestions()
+    user_mood = round((0.6*float(userfeeling))+(0.4*(heartrate)))
     usergoal = input ("how do you want to be feeling on a scale from -3 to 3 ")
     usergoal = int(usergoal)
     return user_mood, usergoal
 
 
-
 def cmm(currentmood, goalmood):
-    aiChoices = defaultaiChoices
+    aiChoices = defaultaiChoices.defaultaiChoices
     if(currentmood<goalmood):
         lightsUp()
         aiChoices = defaultaiChoices.choicesRead()
